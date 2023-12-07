92f4955a74f5a1c4f35f5176fd23a9000b56a9e4
IgorZ <2021testit@gmail.com>
2023-12-07 03:24:34 +0300

    added .secondTest

--- src/test/java/secondTest.java
+++ src/test/java/secondTest.java
@@ -1,6 +1,9 @@
+import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Selenide.open;
 
 public class secondTest {
+   @Test
     void firstTest(){
         open ("https://www.google.com/");
     }
