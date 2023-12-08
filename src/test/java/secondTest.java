1faf3bdc3ff1a3c3e8696af7bbe059a4b557deee
IgorZ <2021testit@gmail.com>
2023-12-08 16:47:06 +0300

    added .allureReportDepend

--- src/test/java/secondTest.java
+++ src/test/java/secondTest.java
@@ -1,10 +1,21 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class secondTest {
-   @Test
+
+    //@BeforeAll
+    static void Configuration(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl="https://www.gismeteo.ru";
+        Configuration.holdBrowserOpen=true;
+    }
+@Test
+        void test () {open("https://www.gismeteo.ru");}
+   //@Test
     void firstTest(){
-        open ("https://www.google.com/");
+        open("/catalog/russia/rostov-oblast/neklinovsky-district/");
     }
 }
